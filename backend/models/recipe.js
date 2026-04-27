7405476d022d82092f5e6f4519902338e12eab37
Janika,Rahikainen <ag3081@student.jamk.fi>
2026-04-27 10:50:44 +0300

    Added user_sub to recipe model

--- backend/models/recipe.js
+++ backend/models/recipe.js
@@ -1,42 +1,53 @@
 const mongoose = require('mongoose');
 
 const RecipeSchema = new mongoose.Schema({
-  name: { 
-    type: String, 
+  user_sub: {
+    type: String,
+    required: true,
+    index: true,
+  },
+  name: {
+    type: String,
     required: [true, 'Reseptin nimi on pakollinen'],
-    trim: true 
+    trim: true,
   },
-  ingredients: [{
-    amount: { type: Number, required: true , min: [0, "Määrä ei voi olla negatiivinen"]},
-    unit: { type: String, trim: true },
-    name: { type: String, required: true, lowercase: true }
-  }],
-  sub: { 
-    type: String, 
-    required: true, 
-    index: true // Nopeuttaa käyttäjän omien reseptien hakua
+  ingredients: [
+    {
+      amount: {
+        type: Number,
+        required: true,
+        min: [0, 'Määrä ei voi olla negatiivinen'],
+      },
+      unit: { type: String, trim: true },
+      name: { type: String, required: true, lowercase: true },
+    },
+  ],
+  sub: {
+    type: String,
+    required: true,
+    index: true,
   },
-  image: { type: String }, // S3 avain
-  created: { 
-    type: Date, 
-    default: Date.now 
+  image: { type: String },
+  created: {
+    type: Date,
+    default: Date.now,
   },
   description: { type: String },
   directions: [{ type: String }],
-  tags: [{ type: String, index: true }], // Indeksi nopeuttaa kategorioittain hakemista
-  servings: { 
-    type: Number, 
-    min: 1, 
-    default: 1 
+  tags: [{ type: String, index: true }],
+  servings: {
+    type: Number,
+    min: 1,
+    default: 1,
   },
-  duration: { 
-    type: Number, 
-    min: 0 // Valmistusaika minuutteina
+  duration: {
+    type: Number,
+    min: 0,
   },
-  public: { 
-    type: Boolean, 
-    default: false 
-  }
+  public: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 module.exports = mongoose.model('Recipe', RecipeSchema, 'recipes');
