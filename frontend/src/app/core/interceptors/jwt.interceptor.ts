f9c52f9e88193ff4f1931b907fcae513a0a59c23
Eetu, Auvinen <ag2389@student.jamk.fi>
2026-04-28 15:21:29 +0300

    frontend ja backend kytketty toisiinsa cognito kirjautumista varten

--- frontend/src/app/core/interceptors/jwt.interceptor.ts
+++ frontend/src/app/core/interceptors/jwt.interceptor.ts
@@ -1,25 +1,34 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
-import { OidcSecurityService } from 'angular-auth-oidc-client';
+import { AuthService } from '../../auth/auth.service';
+import { switchMap } from 'rxjs/operators';
 
 export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
-  const authService = inject(OidcSecurityService);
+  const authService = inject(AuthService);
 
   // Ohita kaikki AWS-pyynnöt
-  if (req.url.includes('amazoncognito.com') || 
-      req.url.includes('cognito-idp') ||
-      req.url.includes('amazonaws.com')) {
+  if (
+    req.url.includes('amazoncognito.com') ||
+    req.url.includes('cognito-idp') ||
+    req.url.includes('amazonaws.com')
+  ) {
     return next(req);
   }
 
-  const token = authService.getAccessToken();
+  // getAccessToken() palauttaa Observablen, joten käytetään rxjs:n switchMapia
+  return authService.getAccessToken().pipe(
+    switchMap((token) => {
+      // console.log('interceptataan pyyntö urliin', req.url);
 
-  if (token && typeof token === 'string') {
-    req = req.clone({
-      setHeaders: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-  }
-  return next(req);
+      if (token) {
+        req = req.clone({
+          setHeaders: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+      }
+
+      return next(req);
+    })
+  );
 };
