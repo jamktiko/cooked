9f0a759f79aad2fa8bdf56657b49a485937b990a
Janika,Rahikainen <ag3081@student.jamk.fi>
2026-04-30 10:46:04 +0300

    Välitallennus

--- frontend/src/app/recipe-add/recipe-add.ts
+++ frontend/src/app/recipe-add/recipe-add.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../models/recipe.model';
 
 @Component({
   selector: 'app-recipe-add',
@@ -7,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-add.css',
 })
 export class RecipeAdd {
+  constructor(private recipeService: RecipeService) {}
 
+  saveNewRecipe() {
+    const newRecipe: Recipe = {
+      title: 'Herkkupasta',
+      ingredients: ['Pasta', 'Tomaatti'],
+      instructions: 'Keitä ja sekoita.',
+      image: 'kuva.jpg',
+    };
+
+    this.recipeService.createRecipe(newRecipe).subscribe({
+      next: (response) => {
+        console.log('Resepti tallennettu onnistuneesti!', response);
+        // Tässä voit esim. ohjata käyttäjän listausnäkymään
+      },
+      error: (err) => {
+        console.error('Tallennus epäonnistui:', err);
+      },
+    });
+  }
 }
