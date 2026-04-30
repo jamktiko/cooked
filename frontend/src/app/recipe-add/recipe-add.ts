19c34b2d79fd3e0fe7778a1673c7ab73b6e853aa
Janika,Rahikainen <ag3081@student.jamk.fi>
2026-04-30 10:31:08 +0300

    Added recipe-add component and recipe model

---
+++ frontend/src/app/recipe-add/recipe-add.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-add',
+  imports: [],
+  templateUrl: './recipe-add.html',
+  styleUrl: './recipe-add.css',
+})
+export class RecipeAdd {
+
+}
