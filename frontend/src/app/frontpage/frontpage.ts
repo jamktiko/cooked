b30f4d2fa74794696e233532e7be4c87a017532a
Eetu Auvinen <ag2389@student.jamk.fi>
2026-05-05 13:14:52 +0300

    mergetty mainista muutokset

--- frontend/src/app/frontpage/frontpage.ts
+++ frontend/src/app/frontpage/frontpage.ts
@@ -2,13 +2,6 @@ import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Recipecard } from '../recipecard/recipecard';
 import { Navbar } from '../navbar/navbar';
-<<<<<<< HEAD
-import { Uploadimg } from '../uploadimg/uploadimg';
-
-@Component({
-  selector: 'app-frontpage',
-  imports: [Recipecard, Navbar, Uploadimg],
-=======
 import { Recipe } from '../models/recipe.model';
 import { RecipeService } from '../services/recipe.service';
 
@@ -16,7 +9,6 @@ import { RecipeService } from '../services/recipe.service';
   selector: 'app-frontpage',
   standalone: true,
   imports: [CommonModule, Recipecard, Navbar],
->>>>>>> main
   templateUrl: './frontpage.html',
   styleUrl: './frontpage.css',
 })
