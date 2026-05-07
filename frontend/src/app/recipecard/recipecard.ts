b66864777a508098f03351c643802b37753957b9
Janika,Rahikainen <ag3081@student.jamk.fi>
2026-05-07 10:54:31 +0300

    Added edit option to recipe card

--- frontend/src/app/recipecard/recipecard.ts
+++ frontend/src/app/recipecard/recipecard.ts
@@ -1,7 +1,7 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Recipe } from '../models/recipe.model';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
 import { S3UrlPipe } from '../pipes/s3-url-pipe';
 
 @Component({
@@ -12,6 +12,7 @@ import { S3UrlPipe } from '../pipes/s3-url-pipe';
   styleUrl: './recipecard.css',
 })
 export class Recipecard {
+  private router = inject(Router);
   @Input() recipe!: Recipe;
   @Input() isOwnRecipe: boolean = false;
   @Input() canDelete: boolean = false;
@@ -20,15 +21,20 @@ export class Recipecard {
   menuOpen = false;
 
   toggleMenu(event: Event) {
-    event.preventDefault();
     event.stopPropagation();
     this.menuOpen = !this.menuOpen;
-    console.log('Valikon tila:', this.menuOpen);
   }
 
   requestDelete(event: Event) {
     event.stopPropagation();
     this.menuOpen = false;
     this.deleteRequest.emit(this.recipe._id);
+  }
+
+  goToEdit(event: Event) {
+    event.stopPropagation();
+    this.menuOpen = false;
+
+    this.router.navigate(['/edit-recipe', this.recipe._id]);
   }
 }
