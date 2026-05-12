2fea92e75c4170aaa5d505bc2cf3b7df5729b143
Eetu, Auvinen <ag2389@student.jamk.fi>
2026-05-12 10:16:54 +0300

    tehty sivutus projektiin

--- frontend/src/app/favorites-page/favorites-page.ts
+++ frontend/src/app/favorites-page/favorites-page.ts
@@ -18,10 +18,33 @@ export class FavoritesPage implements OnInit {
   favorites: any[] = [];
   loading = true;
 
+  // Sivutuksen muuttujat
+  currentPage = 1;
+  totalPages = 1;
+  limit = 9;
+  totalCount = 0; // Kokonaismäärä suosikeille infotekstiä varten
+
   ngOnInit() {
-    this.favoriteService.getFavorites().subscribe({
-      next: (data) => {
-        this.favorites = data;
+    this.loadFavorites();
+  }
+
+  loadFavorites(page: number = 1) {
+    this.currentPage = page;
+    this.loading = true;
+    
+    this.favoriteService.getFavorites(this.currentPage, this.limit).subscribe({
+      next: (data: any) => {
+        if (Array.isArray(data)) {
+          // Vanha fallback taulukolle
+          this.favorites = data;
+          this.totalPages = 1;
+          this.totalCount = data.length;
+        } else {
+          // Uusi objektipohjainen muoto backendistä
+          this.favorites = data.favorites || [];
+          this.totalPages = data.totalPages || 1;
+          this.totalCount = data.totalCount || this.favorites.length;
+        }
         this.loading = false;
       },
       error: (err) => {
@@ -30,10 +53,34 @@ export class FavoritesPage implements OnInit {
       },
     });
   }
-  get favoriteCount(): number {
-    return this.favorites.length;
+
+  nextPage(): void {
+    if (this.currentPage < this.totalPages) {
+      this.loadFavorites(this.currentPage + 1);
+    }
   }
+
+  prevPage(): void {
+    if (this.currentPage > 1) {
+      this.loadFavorites(this.currentPage - 1);
+    }
+  }
+
+  get favoriteCount(): number {
+    return this.totalCount; // Päivitetty palauttamaan tietokannan todellinen määrä, ei pelkän sivun!
+  }
+  
   handleRemoved(recipeId: string) {
     this.favorites = this.favorites.filter((fav) => fav.recipe_id._id !== recipeId);
+    this.totalCount--; // Vähennetään suoraan lukumäärästä jotta UI pysyy ajan tasalla
+    
+    // Voit myös halutessasi hakea listan uusiksi tässä kohtaa,
+    // jos poistat viimeisen elementin sivulta, esim:
+    if (this.favorites.length === 0 && this.currentPage > 1) {
+      this.loadFavorites(this.currentPage - 1);
+    } else if (this.favorites.length < this.limit && this.totalPages > this.currentPage) {
+       // Haetaan data uusiksi jotta sivu täyttyy seuraavan sivun ensimmäisellä itemillä
+      this.loadFavorites(this.currentPage);
+    }
   }
 }
